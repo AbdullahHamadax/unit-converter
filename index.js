61dc06d96513a2df04f58f66d928916302d62b38
AbdullahHamadax <abdallahamada114@gmail.com>
2025-03-24 23:21:40 +0200

    feat: finish design and add starter script

---
+++ index.js
@@ -0,0 +1,6 @@
+const convertBtn = document.getElementById("convert-btn");
+let userValue = document.getElementById("user-input").value;
+
+convertBtn.addEventListener("click", function () {
+  console.log(userValue);
+});
